8d69f8f4f5571f97ad92c8f689dcfc524d365831
frederik.steffen <frederik.steffen@gmail.com>
2021-01-10 15:37:23 +0100

    second contract, random port for stub runner

--- consumer/src/test/java/codes/fdk/sample/contract/consumer/ConsumerApplicationTest.java
+++ consumer/src/test/java/codes/fdk/sample/contract/consumer/ConsumerApplicationTest.java
@@ -4,19 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
 
 @SpringBootTest
-@AutoConfigureStubRunner(stubsMode = LOCAL, ids = "codes.fdk.sample.contract:producer:+:stubs:8081")
+@AutoConfigureStubRunner(stubsMode = LOCAL, ids = "codes.fdk.sample.contract:producer:+:stubs")
 class ConsumerApplicationTest {
 
     @Autowired private ApiClient client;
 
     @Test
-    void name() {
+    void allShouldReturnListOfObjectsFromApi() {
         StepVerifier.create(client.all())
                     .assertNext(first -> assertThat(first.getBar()).isEqualTo("bar"))
                     .assertNext(second -> assertThat(second.getBar()).isEqualTo("baz"))
