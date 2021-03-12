7fcf4662369dddfc99ed06a97fafc1087606b829
frederik.steffen <frederik.steffen@gmail.com>
2021-03-12 14:24:17 +0100

    re-arranged structure

---
+++ producer-side/consumer/src/main/java/codes/fdk/sample/contract/pside/consumer/ApiClient.java
@@ -0,0 +1,26 @@
+package codes.fdk.sample.contract.pside.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@Component
+public class ApiClient {
+
+    private final WebClient client;
+
+    @Autowired
+    public ApiClient(WebClient client) {
+        this.client = client;
+    }
+
+    public Flux<Foo> all() {
+        return client.get().uri("/foo")
+                     .accept(MediaType.APPLICATION_JSON)
+                     .retrieve()
+                     .bodyToFlux(Foo.class);
+    }
+
+}
---
+++ producer-side/consumer/src/main/java/codes/fdk/sample/contract/pside/consumer/ApiClientConfiguration.java
@@ -0,0 +1,37 @@
+package codes.fdk.sample.contract.pside.consumer;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static codes.fdk.sample.contract.pside.consumer.ApiClientConfiguration.ApiClientProperties;
+
+@Configuration
+@EnableConfigurationProperties(ApiClientProperties.class)
+public class ApiClientConfiguration {
+
+    @Bean
+    public WebClient client(WebClient.Builder webClientBuilder, ApiClientProperties config) {
+        return webClientBuilder.baseUrl(config.getBaseUrl())
+                               .build();
+    }
+
+    @ConstructorBinding
+    @ConfigurationProperties("web.client")
+    public static class ApiClientProperties {
+
+        private final String baseUrl;
+
+        public ApiClientProperties(String baseUrl) {
+            this.baseUrl = baseUrl;
+        }
+
+        public String getBaseUrl() {
+            return baseUrl;
+        }
+
+    }
+}
